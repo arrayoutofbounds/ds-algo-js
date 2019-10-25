575c1540d5de9d14054f236946721dc5190508a6
anmoldesai <anmol.desai@lendi.com.au>
2019-10-25 13:59:54 +1100

    add anagram

--- patterns/anagram.js
+++ patterns/anagram.js
@@ -4,23 +4,16 @@ function isAnagram (a,b){
   }
 
   let f1 = {};
-  let f2 = {};
 
   for(let i of a){
     f1[i] = (f1[i] || 0) + 1;
   }
 
-  for(let j of b){
-    f2[j] = (f2[j] || 0) + 1;
-  }
-
-  for(let key in f1){
-    if(!(key in f2)){
+  for(char of b){
+    if(!f1[char]){
       return false;
-    }
-
-    if(!(f1[key] === f2[key])){
-      return false;
+    }else{
+      f1[char] -= 1;
     }
   }
   return true;
