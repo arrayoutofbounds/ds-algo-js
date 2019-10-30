78b95d21f8bbaea4e7cf8f28f6801fd87842c594
Anmol Desai <anmoldesai4@gmail.com>
2019-10-30 20:23:16 +1100

    add unique count of values

---
+++ patterns/pointers/countUnique.js
@@ -0,0 +1,16 @@
+function countUnique(arr){
+    if(arr.length === 0) return 0;
+
+    let i = 0;
+    for(let j = 1; j < arr.length; j++){
+        if(arr[i] !== arr[j]){
+            i++;
+            arr[i] = arr[j];
+        }
+    }
+
+    return i+1;
+}
+
+console.log(countUnique([1,1,2]));
+console.log(countUnique([]));
