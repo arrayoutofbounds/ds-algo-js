3dd81a97d52ddb5468da3815c6da640534200627
Anmol Desai <anmoldesai4@gmail.com>
2019-11-02 15:50:48 +1100

    added bubble sort

--- sorting/bubble.js
+++ sorting/bubble.js
@@ -1,6 +1,6 @@
 function bubble(arr){
-    for(let i = 0; i < arr.length; i++){
-        for(let j = 0; j < arr.length; j++){
+    for(let i = arr.length; i > 0; i--){
+        for(let j = 0; j < i-1; j++){
             if(arr[j] > arr[j+1]){
                 let temp = arr[j];
                 arr[j] = arr[j+1];
@@ -11,4 +11,4 @@ function bubble(arr){
     return arr;
 }
 
-console.log(bubble([5,3,4,1,2]));
+console.log(bubble([5,3,4,1,-2]));
