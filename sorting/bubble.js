8262532f7f25c19fe42b22fd18f23e3cabc4dea5
Anmol Desai <anmoldesai4@gmail.com>
2019-11-02 18:09:53 +1100

    added selection sort

--- sorting/bubble.js
+++ sorting/bubble.js
@@ -1,3 +1,4 @@
+// optimised with no swaps variable.
 function bubble(arr){
     for(let i = arr.length; i > 0; i--){
         for(let j = 0; j < i-1; j++){
@@ -7,6 +8,24 @@ function bubble(arr){
                 arr[j+1] = temp;
             }
         }
+    }
+    return arr;
+}
+
+// optimised with no swaps variable.
+function bubbleOptimised(arr){
+    let noSwaps;
+    for(let i = arr.length; i > 0; i--){
+        noSwaps = true; // assume this
+        for(let j = 0; j < i-1; j++){
+            if(arr[j] > arr[j+1]){
+                let temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+                noSwaps = false; // when we make a swap, update it.
+            }
+        }
+        if (noSwaps) break;
     }
     return arr;
 }
